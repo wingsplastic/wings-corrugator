d43ef4d73288e7aaf3e6a1a5960693ade049c8c8
wingsplastic <info@wingsplastic.com>
2026-02-16 02:53:18 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://你的域名.com', // 等域名解析后替换
+  output: 'static'
 });
